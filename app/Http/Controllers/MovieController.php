41b466df262e79bc29d849fba09f67bd83e5add4
Tommaso-Panarotto <tommaso.panarotto@gmail.com>
2024-10-28 08:58:56 +0100

    set database

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index(){
+        $movies = Movie::all();
+        return view("movie.index", compact("movies"));
+    }
 }
